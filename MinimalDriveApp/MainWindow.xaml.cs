e7856d00503f210792881a8235e025dc925d7bee
dev1gbr <gbrsys@gmail.com>
2026-06-11 22:46:16 +0200

    fix(ui): collapse QAT via VisualTreeHelper traversal
    QuickAccessToolBar.Visibility was null at Loaded time via named property.
    VisualTreeHelper walks the full rendered tree and collapses all instances.

--- MinimalDriveApp/MainWindow.xaml.cs
+++ MinimalDriveApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Media;
 using Fluent;
 
 namespace MinimalDriveApp;
@@ -13,7 +14,18 @@ public partial class MainWindow : RibbonWindow
 
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
-        if (MainRibbon?.QuickAccessToolBar is { } qat)
-            qat.Visibility = Visibility.Collapsed;
+        CollapseAll<QuickAccessToolBar>(this);
+    }
+
+    private static void CollapseAll<T>(DependencyObject parent) where T : UIElement
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+            if (child is T element)
+                element.Visibility = Visibility.Collapsed;
+            else
+                CollapseAll<T>(child);
+        }
     }
 }
